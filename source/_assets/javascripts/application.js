95de2353bf2871ce2a6fae758a258cd858290c8b
guayom <guayo.mena@gmail.com>
2016-08-22 15:27:26 -0600

    Added layout

--- source/_assets/javascripts/application.js
+++ source/_assets/javascripts/application.js
@@ -1,111 +1,32 @@
-console.log('hola');
-var Filter = (function() {
-  function Filter(element) {
-    this._element = $(element);
-    this._optionsContainer = this._element.find(this.constructor.optionsContainerSelector);
-  }
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
 
-  Filter.selector = '.filter';
-  Filter.optionsContainerSelector = '> div';
-  Filter.hideOptionsClass = 'hide-options';
-
-  Filter.enhance = function() {
-    var klass = this;
-
-    return $(klass.selector).each(function() {
-      return new klass(this).enhance();
-    });
-  };
-
-  Filter.prototype.enhance = function() {
-    this._buildUI();
-    this._bindEvents();
-  };
-
-  Filter.prototype._buildUI = function() {
-    this._summaryElement = $('<label></label>').
-      addClass('summary').
-      attr('data-role', 'summary').
-      prependTo(this._optionsContainer);
-
-    this._clearSelectionButton = $('<button class=clear></button>').
-      text('Clear').
-      attr('type', 'button').
-      insertAfter(this._summaryElement);
-
-    this._optionsContainer.addClass(this.constructor.hideOptionsClass);
-    this._updateSummary();
-  };
-
-  Filter.prototype._bindEvents = function() {
-    var self = this;
-
-    this._summaryElement.click(function() {
-      self._toggleOptions();
-    });
-
-    this._clearSelectionButton.click(function() {
-      self._clearSelection();
-    });
-
-    this._checkboxes().change(function() {
-      self._updateSummary();
-    });
-
-    $('body').click(function(e) {
-      var inFilter = $(e.target).closest(self.constructor.selector).length > 0;
-
-      if (!inFilter) {
-        self._allOptionsContainers().addClass(self.constructor.hideOptionsClass);
-      }
-    });
-  };
-
-  Filter.prototype._toggleOptions = function() {
-    this._allOptionsContainers().
-      not(this._optionsContainer).
-      addClass(this.constructor.hideOptionsClass);
-
-    this._optionsContainer.toggleClass(this.constructor.hideOptionsClass);
-  };
-
-  Filter.prototype._updateSummary = function() {
-    var summary = 'All';
-    var checked = this._checkboxes().filter(':checked');
-
-    if (checked.length > 0 && checked.length < this._checkboxes().length) {
-      summary = this._labelsFor(checked).join(', ');
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
     }
 
-    this._summaryElement.text(summary);
-  };
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+});
 
-  Filter.prototype._clearSelection = function() {
-    this._checkboxes().each(function() {
-      $(this).prop('checked', false);
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
     });
-
-    this._updateSummary();
-  };
-
-  Filter.prototype._checkboxes = function() {
-    return this._element.find(':checkbox');
-  };
-
-  Filter.prototype._labelsFor = function(inputs) {
-    return inputs.map(function() {
-      var id = $(this).attr('id');
-      return $("label[for='" + id + "']").text();
-    }).get();
-  };
-
-  Filter.prototype._allOptionsContainers = function() {
-    return $(this.constructor.selector + " " + this.constructor.optionsContainerSelector);
-  };
-
-  return Filter;
-})();
-
-$(function() {
-  Filter.enhance();
+  });
 });
